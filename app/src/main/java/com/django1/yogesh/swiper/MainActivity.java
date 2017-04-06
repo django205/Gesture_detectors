82942e0a9a7ce804557c2dd08ec23c07cf173d8c
Yogesh Kumar <lordyogi1010@gmail.com>
2017-04-06 22:25:44 +0530

    Initial commit

---
+++ app/src/main/java/com/django1/yogesh/swiper/MainActivity.java
@@ -0,0 +1,81 @@
+package com.django1.yogesh.swiper;
+
+import android.support.v4.view.GestureDetectorCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements
+        GestureDetector.OnGestureListener,GestureDetector.OnDoubleTapListener {
+private TextView textView;
+    private GestureDetectorCompat gesturedectector;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+textView=(TextView)findViewById(R.id.textView);
+        this.gesturedectector=new GestureDetectorCompat(this,this);
+        gesturedectector.setOnDoubleTapListener(this);
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent e) {
+        textView.setText("onSingleTapConfirmed");
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent e) {
+        textView.setText("onDoubleTap");
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent e) {
+        textView.setText("onDoubleTapEvent");
+        return true;
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        textView.setText("onDown");
+        return true;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+        textView.setText("onShowPress");
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        textView.setText("onSingleTapUp");
+        return true;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        textView.setText("onScroll");
+        return true;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+        textView.setText("onLongPress");
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        textView.setText("onFling");
+        return true;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        this.gesturedectector.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
+}
